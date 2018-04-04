58a79b293beed5b237bef3d63337d2f52615787a
Alex <fademon2017@gmail.com>
2018-04-04 21:37:31 +0300

    Timer

--- lesson-7/js/script.js
+++ lesson-7/js/script.js
@@ -33,4 +33,44 @@ window.addEventListener('DOMContentLoaded', () => {
 						}
 					}
 				});
+
+				// Timer
+				let deadline = '2018-04-05';
+				function getTimerRemaining(endtime) {
+					let t = Date.parse(endtime) - Date.parse(new Date()),
+									seconds = Math.floor((t / 1000) % 60),
+									minutes = Math.floor((t / 1000 / 60) % 60),
+									hours 		= Math.floor((t / (100 * 60 * 60)) % 60);
+									return {
+										'total'  : t,
+										'hours'  : hours,
+										'minutes': minutes,
+										'seconds': seconds
+									};
+				};
+
+				function setClock(id, endtime) {
+					let timer 		= document.getElementById(id),
+									hours 		= timer.querySelector('.hours'),	
+									minutes = timer.querySelector('.minutes'),
+									seconds = timer.querySelector('.seconds');
+
+									function updateClock() {
+										let	t = getTimerRemaining(endtime);
+										hours.innerHTML = t.hours;
+										minutes.innerHTML = t.minutes;
+										seconds.innerHTML = t.seconds;
+
+										if (t.total <= 0) {
+											clearInterval(timeInt);
+										};
+									};
+
+
+									updateClock();
+									let timeInt = setInterval(updateClock, 1000);
+				};
+
+				setClock('timer', deadline);
+
 });
